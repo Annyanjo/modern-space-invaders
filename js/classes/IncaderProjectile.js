2667f6a356fde33c3f67871fab078afe29a15e17
Annyanjo <anelliz34dosanjosmelloesilva@gmail.com>
2024-05-23 14:53:28 -0400

    add draw function in InvaderProjectile class

--- js/classes/IncaderProjectile.js
+++ js/classes/IncaderProjectile.js
@@ -5,4 +5,9 @@ class InvaderProjectile{
         this.width = 3;
         this.height = 10;
         }
+
+        draw(){
+            c.fillSyle = "white"; 
+            c.fillReact(this.position.x, this.position.y, this.width, this.height);
+        }
     }
