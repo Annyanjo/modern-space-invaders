44b06687fec8a2d42161ac27bcc682b84ca7c1bd
Annyanjo <anelliz34dosanjosmelloesilva@gmail.com>
2024-06-13 16:32:44 -0400

    add fotmatting of project files

--- js/classes/Projectile.js
+++ js/classes/Projectile.js
@@ -1,22 +1,21 @@
-class Projectile{
-    constructor({ position, velocity, color = "red" }){
-        this.position = position;
-        this.velocity = velocity;
-        this.color = color;
-        this.radius = 4;
-    }
+class Projectile {
+  constructor({ position, velocity, color = "red" }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.color = color;
+    this.radius = 4;
+  }
 
-draw(){
+  draw() {
     c.beginPath();
     c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
     c.fill();
     c.closePath();
-}
+  }
 
-
-update(){
+  update() {
     this.draw();
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
-}
+  }
 }
