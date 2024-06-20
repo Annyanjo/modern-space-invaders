fe708dcf38b44e85088652a361aa38b191c57630
Annyanjo <anelliz34dosanjosmelloesilva@gmail.com>
2024-06-20 13:42:09 -0400

    add motivicação projectile

--- js/classes/Projectile.js
+++ js/classes/Projectile.js
@@ -2,13 +2,14 @@ class Projectile {
   constructor({ position, velocity, color = "red" }) {
     this.position = position;
     this.velocity = velocity;
-    this.color = color;
     this.radius = 4;
+    this.color = color;
   }
 
   draw() {
     c.beginPath();
     c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = this.color;
     c.fill();
     c.closePath();
   }
