a045fc134028b38ba2feb3e6c66c44ae1de99d29
Annyanjo <anelliz34dosanjosmelloesilva@gmail.com>
2024-06-20 13:45:30 -0400

    add motivicação particle

--- js/classes/Particle.js
+++ js/classes/Particle.js
@@ -3,16 +3,18 @@ class Particle {
     this.position = position;
     this.velocity = velocity;
     this.radius = radius;
+
     this.color = color;
+
     this.opacity = 1;
     this.fades = fades;
   }
+
   draw() {
     c.save();
     c.globalAlpha = this.opacity;
     c.beginPath();
     c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-    c.closePath();
     c.fillStyle = this.color;
     c.fill();
     c.closePath();
